8c98827d44b1b99cd10484d802bdb2d856c5d45f
Andrey Linnik <andreulinnik@gmail.com>
2022-07-29 10:09:40 +0300

    Создание description.php , описание шаблона

---
+++ local/templates/example/description.php
@@ -0,0 +1,8 @@
+<?php
+
+$arTemplate = array(
+    'NAME' => GetMessage("EXAMPLE_TEMPLATE_NAME"),
+    'DESCRIPTION' => GetMessage("EXAMPLE_TEMPLATE_DESCRIPTION"),
+    'SORT' => 1,
+    'TYPE' => ''
+);
