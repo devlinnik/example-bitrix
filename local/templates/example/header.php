b621b30ad620da904a8bc8900b098e15a73ed184
Andrey Linnik <andreulinnik@gmail.com>
2022-07-29 10:32:24 +0300

    Подключения языковой константы

--- local/templates/example/header.php
+++ local/templates/example/header.php
@@ -36,3 +36,31 @@ Loc::loadMessages(__FILE__);
 <div id="panel">
     <?php $APPLICATION->ShowPanel();?>
 </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/"><?=Loc::getMessage('EXAMPLE_HEADER_BRAND');?></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/">Главная</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Пункт</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Пункт</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Пункт</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Пункт</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid container-global mt-5">
