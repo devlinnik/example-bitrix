d38393c2924edca5a7f7feaa93cf4680cf38094c
Andrey Linnik <andreulinnik@gmail.com>
2022-07-29 10:48:51 +0300

    Реализация подвала

--- local/templates/example/footer.php
+++ local/templates/example/footer.php
@@ -1,2 +1,18 @@
+<?php
+/**
+ * Template footer section
+ */
+
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+    die();
+
+use Bitrix\Main\Localization\Loc as Location;
+
+?>
+<footer class="footer">
+    <div class="container-fluid">
+        <span class="text-muted"><?=Location::getMessage('FOOTER_MESSAGE_1')?></span>
+    </div>
+</footer>
 </body>
 </html>
