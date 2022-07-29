df7c9eec547bb8fe3c32e1e516a6974a041b2cb7
Andrey Linnik <andreulinnik@gmail.com>
2022-07-29 11:29:34 +0300

    Создание класса ExampleNews

--- local/components/example/news/class.php
+++ local/components/example/news/class.php
@@ -1,1 +1,11 @@
 <?php
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * Создание класса ExampleComponent унаследуемого от класса CBitrixComponent
+ */
+class ExampleNews extends CBitrixComponent
+{
+
+}
